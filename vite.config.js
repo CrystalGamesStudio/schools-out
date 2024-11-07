e3db2b596908f838919c96d400df7e92dac69ade
Tomasz Kowalczyk <tomek@kownet.info>
2024-11-07 21:00:03 +0100

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 3000
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    base: '/schools-out',
+    publicDir: 'public'
   }
 }); 
